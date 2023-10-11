b94a4a97d761022bacdd9c16c55c747d9368dbd0
liam04290 <106037383+liam04290@users.noreply.github.com>
2023-10-11 05:55:41 -0400

    Updated typeDefs and Resolvers

--- jobrev/server/graphql/resolvers/index.js
+++ jobrev/server/graphql/resolvers/index.js
@@ -1,20 +1,83 @@
+const { AuthenticationError } = require('apollo-server-express');
 const User = require('../../models/User');
 const Company = require('../../models/Company');
 const Review = require('../../models/Review');
+const { signToken } = require('../../utils/auth');
 
 const resolvers = {
-  Query: {
-    getUsers: async () => {
-      return await User.find();
+    Query: {
+        // Fetch all users
+        users: async () => {
+            return User.find({});
+        },
+        // Fetch single user by ID
+        user: async (_, { userId }) => {
+            return User.findById(userId);
+        },
+        // Fetch all companies
+        companies: async () => {
+            return Company.find({});
+        },
+        // Fetch single company by ID and populate associated reviews
+        company: async (_, { companyId }) => {
+            return Company.findById(companyId).populate('reviews');
+        }
     },
-    getCompanies: async () => {
-      return await Company.find();
-    },
-    getReviews: async () => {
-      return await Review.find();
-    },
-  },
-  // Mutation resolvers for creating, updating, deleting data
+
+    Mutation: {
+        // Register new user and return signed JWT
+        addUser: async (_, { name, email, password }) => {
+            const user = await User.create({ name, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        // Authenticate user given email and password
+        // If good, then return signed JWT
+        login: async (_, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        // Create and return new company
+        addCompany: async (_, { name, description }) => {
+            const company = await Company.create({ name, description });
+            return company;
+        },
+        // Create new review for a company by ID
+        // Requires authenticated user
+        addReview: async (_, { companyId, reviewText, rating }, context) => {
+            if (context.user) {
+                const review = await Review.create({
+                    reviewText,
+                    rating,
+                    companyId,
+                    // Assign review to user
+                    reviewer: context.user._id
+                });
+
+                // Update company to include new review's ID
+                await Company.findByIdAndUpdate(
+                    companyId,
+                    { $push: { reviews: review._id } },
+                    { new: true }
+                );
+
+                return review;
+            }
+            throw new AuthenticationError('You need to be logged in to post a review!');
+        }
+    }
 };
 
 module.exports = resolvers;
--- jobrev/server/graphql/typeDefs/index.js
+++ jobrev/server/graphql/typeDefs/index.js
@@ -1,33 +1,66 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    password: String!  # Avoid password in queries
-  }
 
-  type Company {
-    id: ID!
-    name: String!
-    description: String!
-  }
+# User
 
-  type Review {
-    id: ID!
-    content: String!
-    userId: ID!
-    companyId: ID!
-  }
+type User {
+  _id: ID!
+  name: String!
+  email: String!
+  password: String! 
+}
 
-  type Query {
-    getUsers: [User]
-    getCompanies: [Company]
-    getReviews: [Review]
-  }
+type Auth {
+  token: String!
+  user: User!
+}
 
-  # Mutations for createUser, createCompany...
+type Query {
+  me: User
+  users: [User]
+  user(userId: ID!): User
+  companies: [Company]
+  company(companyId: ID!): Company
+  reviews(companyId: ID!): [Review]
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(name: String!, email: String!, password: String!): Auth
+}
+
+# Company
+
+type Company {
+  _id: ID!
+  name: String!
+  description: String!
+  reviews: [Review]
+}
+
+extend type Mutation {
+  addCompany(name: String!, description: String!): Company
+  updateCompany(companyId: ID!, name: String, description: String): Company
+  deleteCompany(companyId: ID!): String
+}
+
+# Review
+
+type Review {
+  _id: ID!
+  reviewText: String!
+  rating: Int!
+  createdAt: String
+  company: Company
+}
+
+extend type Mutation {
+  addReview(companyId: ID!, reviewText: String!, rating: Int!): Review
+  updateReview(reviewId: ID!, reviewText: String, rating: Int): Review
+  deleteReview(reviewId: ID!): String
+}
+
 `;
 
 module.exports = typeDefs;
