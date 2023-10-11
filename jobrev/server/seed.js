8c347ae202dd1d549fa7185a6239f49266456542
liam04290 <106037383+liam04290@users.noreply.github.com>
2023-10-11 19:03:49 -0400

    Fixing seeds

--- jobrev/server/seed.js
+++ jobrev/server/seed.js
@@ -7,57 +7,75 @@ const bcrypt = require('bcryptjs');
 
 const saltRounds = 10;
 
-
-mongoose.connect(process.env.MONGODB_URI , {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Clear out existing data
 async function clearDatabase() {
-    await mongoose.connection.dropCollection('users');
-    await mongoose.connection.dropCollection('companies');
-    await mongoose.connection.dropCollection('reviews');
+    try {
+        await mongoose.connection.dropCollection('users');
+    } catch (error) {
+        console.log('Users collection not found.');
+    }
+
+    try {
+        await mongoose.connection.dropCollection('companies');
+    } catch (error) {
+        console.log('Companies collection not found.');
+    }
+
+    try {
+        await mongoose.connection.dropCollection('reviews');
+    } catch (error) {
+        console.log('Reviews collection not found.');
+    }
 }
 
-clearDatabase().then(() => {
+async function seedDatabase() {
+    await clearDatabase();
 
-// User data
-const users = [
-  { name: 'Alice', email: 'alice@email.com', password: 'password123' },
-  { name: 'Bob', email: 'bob@email.com', password: 'password123' },
-];
-
-// Hash passwords
-for (let user of users) {
-    user.password = bcrypt.hashSync(user.password, saltRounds);
-}
-
-// Insert Users
-User.insertMany(users)
-  .then(insertedUsers => {
-    // Company data
-    const companies = [
-      { name: 'MegaCorp', description: 'Not nice.', user: insertedUsers[0]._id },
-      { name: 'Small Business', description: 'Nice.', user: insertedUsers[1]._id },
+    // User data
+    const usersData = [
+        { name: 'Alice', email: 'alice@email.com', password: 'password123' },
+        { name: 'Bob', email: 'bob@email.com', password: 'password123' },
     ];
 
-    // Insert Companies
-    return Company.insertMany(companies)
-      .then(insertedCompanies => {
-        // Review data
-        const reviews = [
-          { reviewText: 'Did not vibe.', rating: 2, companyId: insertedCompanies[0]._id, reviewer: insertedUsers[0]._id },
-          { reviewText: 'Vibed.', rating: 5, companyId: insertedCompanies[1]._id, reviewer: insertedUsers[1]._id },
-        ];
+    // Hash passwords
+    const usersWithHashedPasswords = usersData.map(user => ({
+        ...user,
+        password: bcrypt.hashSync(user.password, saltRounds)
+    }));
 
-        // Insert Reviews
-        return Review.insertMany(reviews);
-      });
-  })
-  .then(() => {
-    console.log('Data seeded!');
-    mongoose.connection.close();
-  })
-  .catch((error) => console.log(error));
-});
+    const insertedUsers = await User.insertMany(usersWithHashedPasswords);
+
+    // Company data
+    const companiesData = [
+        { name: 'MegaCorp', description: 'Not nice.', user: insertedUsers[0]._id },
+        { name: 'Small Business', description: 'Nice.', user: insertedUsers[1]._id },
+    ];
+
+    const insertedCompanies = await Company.insertMany(companiesData);
+
+    // Review data
+    const reviewsData = [
+        { reviewText: 'Did not vibe.', rating: 2, companyId: insertedCompanies[0]._id, reviewer: insertedUsers[0]._id },
+        { reviewText: 'Vibed.', rating: 5, companyId: insertedCompanies[1]._id, reviewer: insertedUsers[1]._id },
+    ];
+
+    await Review.insertMany(reviewsData);
+}
+
+async function main() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        await seedDatabase();
+
+        console.log('Database seeded!');
+    } catch (error) {
+        console.error(error);
+    } finally {
+        mongoose.connection.close();
+    }
+}
+
+main();
